0ae6d5e52649840332ae1df264ad7f5f067c16d6
prernacollegengp-gif <prernacollegengp@gmail.com>
2026-05-30 16:08:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const text = [
+"DevOps Engineer",
+"Python Developer",
+"AWS Engineer",
+"Docker Specialist",
+"Kubernetes Enthusiast"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+
+count++;
+index = 0;
+
+setTimeout(type, 1000);
+
+}else{
+
+setTimeout(type, 100);
+
+}
+
+})();
